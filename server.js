66b3a2b2577264d95ae8f7273c1d5ba779283d39
Muhammad Ahmed <ahmed.hanjra96@gmail.com>
2019-08-01 18:56:27 +0500

    second commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app=express();
+
+app.use(express.static(__dirname +'/dist'));
+app.get('/assets',function(req,res){
+  res.sendFile(__dirname+'/dist/assets');
+});
+app.get('/*',function(req,res){
+  res.sendFile(__dirname+'/dist/index.html');
+});
+
+var port = process.env.port || 8080;
+app.listen(port,function(){
+  console.log('server running on port' + port +'.');
+})
